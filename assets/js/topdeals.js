6b59262fc13a3fe07aed5a975468e1cb49e481e0
manojecs2020 <manoj@ecscloudinfotech.com>
2024-10-08 18:30:17 +0530

    topdeals added

---
+++ assets/js/topdeals.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      dots: false,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        768: {
+          items: 3,
+        },
+        1000: {
+          items: 5,
+        },
+        1500: {
+          items: 5,
+        },
+      },
+    });
+  });
