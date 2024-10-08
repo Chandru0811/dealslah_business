cb76f055d6ae12534bf1a683b1fd5fc2e3c01e10
Chandru0811 <chandru08112000@gmail.com>
2024-10-08 23:11:45 +0530

    UI Corrected

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,25 @@
-console.log("Script is Working")
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+      1500: {
+        items: 5,
+      },
+    },
+  });
+});
